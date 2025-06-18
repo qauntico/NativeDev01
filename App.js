01db79a0d4e4ec0d876eb60f624ff6cc7ddbc818
qauntico <bauketfabrice10@gmail.com>
2025-06-18 22:42:10 +0100

    reseting project

--- App.js
+++ App.js
@@ -1,61 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
-import CategoriesScreen from "./screens/categoryScreen";
-import { NavigationContainer } from "@react-navigation/native";
+
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import MealsOverviewScreen from "./screens/MealsOverviewScreen";
-import { StatusBar } from "expo-status-bar";
-import MealDetail from "./screens/MealDetail";
+
 import { createDrawerNavigator } from "@react-navigation/drawer";
-import FavoriteScreens from "./screens/FavoriteScreens";
-import FavoritesContextProvider from "./store/context/favorite-context";
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 
-function DrawerNavigator() {
-  return (
-    <Drawer.Navigator
-      screenOptions={{
-        headerStyle: { backgroundColor: "#351401" },
-        headerTintColor: "white",
-        contentStyle: { backgroundColor: "#3f2f25" },
-      }}
-    >
-      <Drawer.Screen name="Categories" component={CategoriesScreen} />
-      <Drawer.Screen name="Favorite" component={FavoriteScreens} />
-    </Drawer.Navigator>
-  );
-}
 export default function App() {
-  return (
-    <View style={styles.rootScreen}>
-      <StatusBar />
-      <FavoritesContextProvider>
-        <NavigationContainer>
-          <Stack.Navigator
-            screenOptions={{
-              headerStyle: { backgroundColor: "#351401" },
-              headerTintColor: "white",
-              contentStyle: { backgroundColor: "#3f2f25" },
-            }}
-          >
-            <Stack.Screen
-              name="MealsCategories"
-              component={DrawerNavigator}
-              options={{
-                headerShown: false, //can be used to remove the parent header
-              }}
-            />
-            <Stack.Screen
-              name="MealsOverview"
-              component={MealsOverviewScreen}
-            />
-            <Stack.Screen name="MealDetail" component={MealDetail} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </FavoritesContextProvider>
-    </View>
-  );
+  return <View style={styles.rootScreen}></View>;
 }
 
 const styles = StyleSheet.create({
