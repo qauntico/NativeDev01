b6104b68713f0ba1f38c2ad64fffba8a21b412db
qauntico <bauketfabrice10@gmail.com>
2025-07-02 20:04:16 +0100

    changes in app structure

---
+++ components/places/PlaceItem.js
@@ -0,0 +1,17 @@
+import { View, Text, Pressable, StyleSheet } from 'react-native'
+
+export default function PlaceItem({place, onSelect}) {
+    return (
+        <Pressable onPress={onSelect}>
+            <Image source={{uri: place.imageUri}} />
+            <View>
+                <Text>{place.title}</Text>
+                <Text>{place.address}</Text>
+            </View>
+        </Pressable>
+    )
+}    
+
+const styles = StyleSheet.create({
+  
+})  
---
+++ screens/AllPlace.js
@@ -0,0 +1,8 @@
+import { View, Text } from 'react-native'
+import PlacesList from '../components/places/PlacesList'
+
+export default function AllPlace() {
+    return (
+        <PlacesList places={[]} />
+    )
+}   
