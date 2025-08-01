11c5083361484baf9a09bf641fa21c56a31b6324
qauntico <bauketfabrice10@gmail.com>
2025-08-01 13:47:22 +0100

    Add Google Maps API key configuration and update dependencies

--- UI/location.js
+++ UI/location.js
@@ -1,7 +1,10 @@
-const GOOGlE_MAPS_API_KEY = "AIzaSyAdjiJKX8oFANs6idNeeI8TjZ94CAxqqTg";
+import Constants from 'expo-constants';
+const GOOGLE_MAPS_KEY =
+  Constants?.expoConfig?.extra?.GOOGLE_MAPS_KEY
 export default function getMapPreview(lat, lng) {
+    console.log("Google Maps API Key:", GOOGLE_MAPS_KEY);
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap
-&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGlE_MAPS_API_KEY}`;
+&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_MAPS_KEY}`;
   return imagePreviewUrl;
 }
 
---
+++ app.config.js
@@ -0,0 +1,9 @@
+export default {
+  expo: {
+    name: "myapp",
+    slug: "myapp",
+    extra: {
+      GOOGLE_MAPS_KEY: process.env.GOOGLE_MAPS_KEY,
+    },
+  },
+};
